7cb394e3d46af314821765243217138bf9180773
Piotr <marsz.piotr@gmail.com>
2022-11-27 20:00:09 +0100

    working

--- rekrutacja/weatherweb/views.py
+++ rekrutacja/weatherweb/views.py
@@ -2,10 +2,12 @@ import os
 
 import requests
 from django.contrib import messages
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views import View
 
 from weatherweb.forms import CityForm
+from weatherweb.models import WeatherCity
 
 
 # Create your views here.
@@ -25,32 +27,45 @@ class CityWeather(View):
 
     def post(self, request):
         form = CityForm(request.POST)
-        form_handler = None
+        new_city_weather = None
 
         if form.is_valid():
             self.city = form.cleaned_data['city']
             self.lon = form.cleaned_data['lon']
             self.lat = form.cleaned_data['lat']
-            form_handler = form.save(commit=False)
-            if 'favourite' in request.POST:
-                form_handler.user = request.user
-                messages.success(request, "Added to favourites")
+
+            db_city = self.check_city_in_db()
+            if db_city:
+                new_city_weather = db_city
             else:
-                if self.city and not (self.lon or self.lat):
-                    self._get_coordinates_from_city(request)
-                    form_handler.lon = self.lon
-                    form_handler.lat = self.lat
+                new_city_weather = form.save(commit=False)
+                if 'favourite' in request.POST:
+                    new_city_weather.user = request.user
+                    messages.success(request, "Added to favourites")
+                else:
+                    if self.city and not (self.lon or self.lat):
+                        self._get_coordinates_from_city(request)
+                        new_city_weather.lon = self.lon
+                        new_city_weather.lat = self.lat
 
-                elif (self.lon and self.lat) and not self.city:
-                    self._get_city_from_coordinates(request)
-                    form_handler.city = self.city
-            form_handler.save()
+                    elif (self.lon and self.lat) and not self.city:
+                        self._get_city_from_coordinates(request)
+                        new_city_weather.city = self.city
 
-        self.ctx['forecast'] = self._get_forecast()
-        self.ctx['weather'] = self._get_weather()
-        self.ctx['city'] = self.city
-        self.ctx['form'] = CityForm(instance=form_handler)
+                new_city_weather.weather = self._get_weather()
+                new_city_weather.forecast = self._get_forecast()
+                new_city_weather.save()
+
+        self.ctx['data'] = new_city_weather
+        self.ctx['form'] = CityForm(instance=new_city_weather)
         return render(request, "weather.html", self.ctx)
+
+    def check_city_in_db(self):
+        try:
+            weather_city = WeatherCity.objects.filter(Q(city=self.city) | Q(lon=self.lon, lat=self.lat)).latest('update_at')
+        except WeatherCity.DoesNotExist:
+            weather_city = None
+        return weather_city
 
     def _get_forecast(self):
         url = 'https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&units={}&cnt={}&appid={}'
